2a017f30d6d464bbf5cadcf78fbfe80aaba985c7
Rubén <rubenalb90@gmail.com>
2018-06-24 01:05:10 +0200

    Telegram Users in admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import TelegramUser
+
 # Register your models here.
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'username',
+        'first_name',
+        'last_name',
+        'last_command',
+        'country',
+        'last_activity',
+        'created',
+    )
+
+    search_fields = ('username',)
+
+admin.site.register(TelegramUser, TelegramUserAdmin)
